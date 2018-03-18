c220f8c436534ad6be8360a42fe7ec4651bc1c9d
fariz.tumbuan <13515050@std.stei.itb.ac.id>
2018-03-18 17:01:51 +0800

    Init commit (basic empty window)

---
+++ basic_template/test/main.cpp
@@ -0,0 +1,43 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <iostream>
+
+// Function for resizing window. Put in "glutReshapeFunc"
+void changeViewPort(int w, int h) {
+	glViewport(0, 0, w, h);
+}
+
+// the "paint" method
+void render() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
+	// Initializes GLUT
+	glutInit(&argc, argv);
+
+	// Set up some memory buffers for our display
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+
+	// Set window size
+	glutInitWindowSize(800, 600);
+
+	// Create the window with the title "Hello Fariz"
+	glutCreateWindow("Grafika");
+
+	// Bind the two functions (above) to respond when necessary
+	glutReshapeFunc(changeViewPort);
+	glutDisplayFunc(render);
+
+	// Needed to call OpenGL functions
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		fprintf(stderr, "GLEW Error");
+		return 1;
+	}
+
+	// Start up the loop
+	glutMainLoop();
+	return 0;
+}
