9903e1805289c81160ab653d15b3e7ac43ef6a88
fariz.tumbuan <13515050@std.stei.itb.ac.id>
2018-03-18 17:45:17 +0800

    Added static file reader function in basic_template

--- basic_template/test/main.cpp
+++ basic_template/test/main.cpp
@@ -4,6 +4,34 @@
 
 const std::string WINDOW_TITLE = "BASIC TEMPLATE";
 
+// Static utility function for reading files and returns string. Used for reading .vsh and .vsf
+static char* readFile(const char* filename) {
+	FILE* fp;
+	errno_t err = fopen_s(&fp, filename, "r");
+
+	if (err) {
+		printf("Error opening "); printf(filename); printf("\n");
+	}
+
+	fseek(fp, 0, SEEK_END);
+
+	long file_length = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	char *contents = new char[file_length + 1];
+
+	for (int i = 0; i < file_length + 1; i++) {
+		contents[i] = 0;
+	}
+
+	fread(contents, 1, file_length, fp);
+
+	contents[file_length + 1] = '\0';
+	fclose(fp);
+
+	return contents;
+}
+
 // Function for resizing window. Put in "glutReshapeFunc"
 void changeViewPort(int w, int h) {
 	glViewport(0, 0, w, h);
